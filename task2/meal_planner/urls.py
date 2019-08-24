13d8537d0b71c7b615886fda520db80e9e71bd52
Abhishek Juneja <abhishek.juneja@bizongo.com>
2019-08-24 17:47:28 +0530

    Django, DRF, Swagger, Jet, Logging, Django Dev helpers setup

---
+++ task2/meal_planner/urls.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include, url
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
+
+from meal_planner.settings.base import get_env_variable
+
+
+schema_view = get_swagger_view(title='Meal Planner Swagger API')
+
+# project level imports
+from meal_planner.settings.base import ADMIN_SITE_HEADER
+
+
+admin.site.site_header = ADMIN_SITE_HEADER
+
+urlpatterns = [
+    url(r'^', admin.site.urls),
+    url(r'^jet/', include('jet.urls', 'jet')),
+    # url(r'^auth/login/$', obtain_jwt_token),
+    path(r'api-docs/', include_docs_urls(title='Meal Planner API')),
+    path(r'swagger-docs/', schema_view),
+]
+
+
+if get_env_variable('DEBUG_TOOLBAR'):
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
